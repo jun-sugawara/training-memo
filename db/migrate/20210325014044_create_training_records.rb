f9a7eab7d8e498c0fb5af9b71013ecdb02a2ae58
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-25 10:55:29 +0900

    training_recordモデル作成

---
+++ db/migrate/20210325014044_create_training_records.rb
@@ -0,0 +1,13 @@
+class CreateTrainingRecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :training_records do |t|
+      t.integer    :date,            null: false
+      t.integer    :training_event,  null: false
+      t.integer    :training_weight, null: false
+      t.integer    :reps,            null: false
+      t.integer    :set,             null: false 
+      # t.references :user             foreign_key: true
+      t.timestamps
+    end
+  end
+end
