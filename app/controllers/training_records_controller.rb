6e18d6dad3ab6a74f8a05259bbac1e71ec1a0499
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-27 22:14:23 +0900

    トレーニングコントローラー準備

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -1,4 +1,18 @@
 class TrainingRecordsController < ApplicationController
   def index
+    @training_records = TrainingRecord.all
+  end
+
+  def new
+    @training_record = TrainingRecord.new
+  end
+
+  def create
+    TrainingRecord.create(trainingrecord_params)
+  end
+
+  private
+  def trainingrecord_params
+    params.require(:training_record).permit(:date, :training_event, :training_weight, :reps, :set)
   end
 end
