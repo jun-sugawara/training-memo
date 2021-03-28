efbb20b1681a27333eb917cb984f6976ffa4ada1
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-28 14:47:21 +0900

    コントローラー実装
    ・destroy,edit,updateアクションと処理を実装
    ・同じ処理をbefore_acitionにまとめ

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -1,4 +1,6 @@
 class TrainingRecordsController < ApplicationController
+before_action :set_training_record, only: [:destroy, :edit, :update]
+
   def index
     @training_records = TrainingRecord.all
   end
@@ -11,8 +13,24 @@ class TrainingRecordsController < ApplicationController
     TrainingRecord.create(trainingrecord_params)
   end
 
+  def destroy
+    @training_record.destroy
+  end
+ 
+  def edit
+  end
+
+  def update
+    @training_record.update(trainingrecord_params)
+  end
+
   private
+
   def trainingrecord_params
     params.require(:training_record).permit(:date, :training_event, :training_weight, :reps, :set)
+  end
+
+  def set_training_record
+    @training_record = TrainingRecord.find(params[:id])
   end
 end
