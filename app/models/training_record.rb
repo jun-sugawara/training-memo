e65cc5bfb6908787a81ef747e24e2f08bdc3f2c1
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-28 09:43:51 +0900

    バリデーション設定

--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,2 +1,12 @@
 class TrainingRecord < ApplicationRecord
+  #アソシエーション記述
+
+  with_options presence: true do
+    validates :date
+    validates :training_event
+    validates :training_weight
+    validates :reps
+    validates :set
+  end
+
 end
