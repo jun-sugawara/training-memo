dc43726b2f4f41c252aa7d22592a3c65857b8ee2
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-03-30 10:24:24 +0900

    userのマイグレーションファイル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_060127) do
+ActiveRecord::Schema.define(version: 2021_03_30_001015) do
 
-  create_table "body_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "body_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "date", null: false
     t.integer "body_weight", null: false
     t.integer "fat", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_060127) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "training_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "training_records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "date", null: false
     t.integer "training_event", null: false
     t.integer "training_weight", null: false
@@ -29,6 +29,21 @@ ActiveRecord::Schema.define(version: 2021_03_28_060127) do
     t.integer "set", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.integer "age", null: false
+    t.integer "gender", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
