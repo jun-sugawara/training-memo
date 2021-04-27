b5286dd673140b777df65d8c0b9d0693e7dd2019
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-04-27 19:15:43 +0900

    他のユーザーのページへのアクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+before_action :move_to_top
+
 
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
   end
+
+
+  private
+
+  def move_to_top
+    @user = User.find(params[:id])
+    redirect_to root_path unless @user == current_user
+  end
+
 end
