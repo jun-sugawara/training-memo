77a1d92cd3cdd84f16abf537ef8edb4138c37c43
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-05-06 12:00:35 +0900

    シングルクォーテーションへ変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,17 +16,17 @@ RSpec.describe User, type: :model do
       it 'ニックネームが空では登録ができない' do
         @user.nickname = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "ニックネームを入力してください"
+        expect(@user.errors.full_messages).to include 'ニックネームを入力してください'
       end
       it 'ニックネームが11文字以上では登録ができない' do
         @user.nickname = '11文字以上のニックネーム'
         @user.valid?
-        expect(@user.errors.full_messages).to include "ニックネームは10文字以内で入力してください"
+        expect(@user.errors.full_messages).to include 'ニックネームは10文字以内で入力してください'
       end
       it 'メールアドレスが空では登録ができない' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "Eメールを入力してください"
+        expect(@user.errors.full_messages).to include 'Eメールを入力してください'
       end
       it 'メールアドレスに@が含まれていないと登録ができない' do
         @user.email = 'test.com'
@@ -43,12 +43,12 @@ RSpec.describe User, type: :model do
       it 'パスワードが空では登録ができない' do
         @user.password = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "パスワードを入力してください"
+        expect(@user.errors.full_messages).to include 'パスワードを入力してください'
       end
       it 'パスワードが全角英数字混合では登録ができない' do
         @user.password = 'A１A２A３'
         @user.valid?
-        expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
+        expect(@user.errors.full_messages).to include 'パスワード（確認用）とパスワードの入力が一致しません'
       end
       it 'パスワードが５文字以下では登録ができない' do
         @user.password = 'a1a1a'
@@ -68,22 +68,22 @@ RSpec.describe User, type: :model do
       it 'パスワードとパスワード（確認）が一致しなければ登録ができない' do
         @user.password_confirmation = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "パスワード（確認用）とパスワードの入力が一致しません"
+        expect(@user.errors.full_messages).to include 'パスワード（確認用）とパスワードの入力が一致しません'
       end
       it '年齢が空では登録ができない' do
         @user.age = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "年齢を入力してください"
+        expect(@user.errors.full_messages).to include '年齢を入力してください'
       end
       it '年齢は半角数字でなければ登録ができない' do
         @user.age = '２６'
         @user.valid?
-        expect(@user.errors.full_messages).to include "年齢は数値で入力してください"
+        expect(@user.errors.full_messages).to include '年齢は数値で入力してください'
       end
       it '性別が選択されていなければ登録ができない' do
         @user.gender = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "性別を入力してください"
+        expect(@user.errors.full_messages).to include '性別を入力してください'
       end
     end
   end
