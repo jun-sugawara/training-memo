de0868352ff6b0641eb53b46358da0a3e6f3b0a9
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-05-10 22:15:47 +0900

    FactoryBot日付修正

--- spec/factories/body_records.rb
+++ spec/factories/body_records.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :body_record do
     association :user
 
-    date             { 2021 - 0o5 - 0o1 }
+    date             { '2021/05/01' }
     body_weight      { 60.5 }
     fat              { 18.5 }
     todays_condition { '良い' }
