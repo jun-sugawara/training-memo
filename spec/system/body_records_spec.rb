dde243791ea4c4f3f53b05049c97f97c2e7eaf19
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-05-10 22:17:38 +0900

    体組成記録機能の結合テスト記述

--- spec/system/body_records_spec.rb
+++ spec/system/body_records_spec.rb
@@ -3,23 +3,40 @@ require 'rails_helper'
 RSpec.describe '体組成記録保存', type: :system do
   before do
     @body_record = FactoryBot.build(:body_record)
-  end
-  context '体組成の保存ができる時' do
-    it 'ログインしたユーザーは体組成の記録を保存することができる' do
-      # ログインする
-      visit new_user_session_path
-      # マイページに遷移する
-
-      # 体組成保存ページに遷移する
-      # フォームに情報を入力する
-      # 送信するとBodyRecordモデルのカウントが１上がることを確認する
-      # 保存が完了しましたの文字があることを確認する
-    end
+    @user = FactoryBot.create(:user)
   end
   context '体組成の保存ができない時' do
     it 'ログインしていないと体組成ページへ遷移できない' do
       # トップページへ遷移する
+      visit root_path
       # 体組成保存ページへのリンクがない
+      expect(page).to have_no_content("マイページへ進む")
+    end
+  end
+  context '体組成の保存ができる時' do
+    it 'ログインしたユーザーは体組成の記録を保存することができる' do
+      # # ログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      click_on('ログインする')
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('マイページへ進む')
+      # マイページに遷移する
+      visit user_path(@user.id)
+      expect(page).to have_content('体重・体脂肪を記録する')
+      # 体組成保存ページに遷移する
+      visit new_body_record_path
+      # フォームに情報を入力する
+      fill_in 'body_record_date', with: @body_record.date
+      fill_in 'body_record_body_weight', with: @body_record.body_weight
+      fill_in 'body_record_fat', with: @body_record.fat
+      # 送信するとBodyRecordモデルのカウントが１上がることを確認する
+      expect {
+        click_on('保存する')
+      }.to change { BodyRecord.count }.by(1)
+      # 保存が完了しましたの文字があることを確認する
+      expect(page).to have_content('保存が完了しました')
     end
   end
 end
