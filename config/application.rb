7765bef5cf3ceb050ba3a673e8578d6da2f7eaa1
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-05-17 21:47:50 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module TrainingMemo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
-    config.assets.initialize_on_precompile = false
-    config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
-      html_tag
-    end
+    # config.assets.initialize_on_precompile = false
+    # config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
+    #   html_tag
+    # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
